0834c85b3e4aa501fb65e73d95256c191d6f3c12
anjaritullahid88 <anjaritullah.id@gmail.com>
2026-03-26 17:50:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let slides = document.querySelectorAll('.slide');
+
+let index = 0;
+
+function nextSlide(){
+slides[index].classList.remove('active');
+
+index++;
+
+if(index >= slides.length){
+index = 0;
+}
+
+slides[index].classList.add('active');
+}
+
+setInterval(nextSlide,4000);
